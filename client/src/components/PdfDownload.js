f1e20f09ddca2ba8aaaf96ec2318c6d4b80ca7ad
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2020-05-24 02:33:25 +0700

    put format of form pdf

--- client/src/components/PdfDownload.js
+++ client/src/components/PdfDownload.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactToPdf from 'react-to-pdf';
 import moment from 'moment';
 import _ from 'lodash';
@@ -8,6 +8,12 @@ import { Button, Container, Form, Row, Col, Table } from 'react-bootstrap';
 import { formatNumber, printStrForm, printDate, printStr, inWords } from '../helpers'
 
 function PdfDownload({ data }) {
+  const [hostName, setHostName] = useState('');
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [discountFinal, setDiscountFinal] = useState(0);
+  const [delivery, setDelivery] = useState(0);
+
   const ref = React.createRef();
   const options = {
     orientation: 'potrait',
@@ -43,6 +49,14 @@ function PdfDownload({ data }) {
     return momentEnd.diff(momentStart, perimeter, true);
   }
 
+  function handleDiscountFinal(e) {
+    if (Number(e.target.value) > subTotal() + otherTotal() + delivery) {
+      setDiscountFinal(0);
+    } else {
+      setDiscountFinal(e.target.value);
+    }
+  }
+
   function subTotal() {
     return (100 - data.discount) / 100 * data[data.type] * timeDiff();
   }
@@ -56,18 +70,18 @@ function PdfDownload({ data }) {
   }
 
   function finalWords() {
-    const formatted = formatNumber(subTotal());
+    const formatted = formatNumber(finalTotal());
     const splitted = splitPointNumber(formatted);
 
     return `${inWords(splitted[0]) || 'nol'} koma ${inWords(splitted[1]) || 'nol'}`
   }
 
   function taxPrice() {
-    return data.tax / 100 * data.price;
+    return data.tax / 100 * total();
   }
 
   function otherPriceConverter() {
-    return _.map(_.get(data, 'otherExpenses', []), item => item.price);
+    return _.map(_.get(data, 'otherExpenses', []), item => Number(item.price));
   }
 
   function otherTotal() {
@@ -75,7 +89,11 @@ function PdfDownload({ data }) {
   }
 
   function total() {
-    return subTotal() + otherTotal() + taxPrice() + data.delivery || 0 - data.discountFinal || 0
+    return subTotal() + otherTotal() + Number(delivery) - Number(discountFinal)
+  }
+
+  function finalTotal() {
+    return total() + taxPrice();
   }
 
   return (
@@ -109,8 +127,8 @@ function PdfDownload({ data }) {
               <Col className="d-flex flex-column" md="5">
                 <span>: {printStrForm(data._id)}</span>
                 <span>: {printStrForm(data.customer)}</span>
-                <span>: {printStrForm(data.pic)}</span>
-                <span>: {printStrForm(data.address)}</span>
+                <span>: {printStrForm(name)}</span>
+                <span>: {printStrForm(address)}</span>
               </Col>
               <Col className="d-flex flex-column" md="2">
                 <span>Tanggal</span>
@@ -179,26 +197,26 @@ function PdfDownload({ data }) {
                     <span>Discount Final</span>
                     <span>Pajak</span>
                     <span>Biaya Pengantaran</span>
-                    <span>Total</span>
+                    <span><strong>Total</strong></span>
                   </Col>
                   <Col className="d-flex flex-column" md="2">
-                    <span>: Rp</span>
-                    <span>: Rp</span>
-                    <span>: Rp</span>
-                    <span>: Rp</span>
+                    <span>: - Rp</span>
+                    <span>: + Rp</span>
+                    <span>: + Rp</span>
+                    <span><strong>: Rp</strong></span>
                   </Col>
                   <Col className="d-flex flex-column" md="5">
                     <Row className="justify-content-end">
-                      <span>{printStrForm(formatNumber(data.discountFinal || 0))}</span>
+                      <span>{printStrForm(formatNumber(discountFinal || 0))}</span>
                     </Row>
                     <Row className="justify-content-end">
                       <span>{printStrForm(formatNumber(taxPrice()))}</span>
                     </Row>
                     <Row className="justify-content-end">
-                      <span>{printStrForm(formatNumber(data.delivery || 0))}</span>
+                      <span>{printStrForm(formatNumber(delivery || 0))}</span>
                     </Row>
                     <Row className="justify-content-end">
-                      <span>{printStrForm(formatNumber(total()))}</span>
+                      <span><strong>{printStrForm(formatNumber(finalTotal()))}</strong></span>
                     </Row>
                   </Col>
                 </Row>
@@ -208,9 +226,12 @@ function PdfDownload({ data }) {
               <div className="my-2" />
             </Row>
             <Row className="justify-content-around" style={{ fontSize: 12 }}>
-              <Col md="4" style={{ height: '30mm' }} className="border-bottom">
+              <Col md="4" style={{ height: '30mm' }} className="border-bottom d-flex flex-column justify-content-between">
                 <Row className="justify-content-center">
                   <span>Customer</span>
+                </Row>
+                <Row className="justify-content-center">
+                  <span>{name}</span>
                 </Row>
               </Col>
               <Col md="4" style={{ height: '30mm' }} className="border-bottom d-flex flex-column justify-content-between">
@@ -218,14 +239,14 @@ function PdfDownload({ data }) {
                   <span>PT. Trans Pacific Global</span>
                 </Row>
                 <Row className="justify-content-center">
-                  <span>Employee Name</span>
+                  <span>{hostName}</span>
                 </Row>
               </Col>
             </Row>
             <Row className="justify-content-around" style={{ fontSize: 12 }}>
               <Col md="4">
                 <Row className="justify-content-center">
-                  <span>Someone You Know</span>
+                  <span>{data.customer}</span>
                 </Row>
               </Col>
               <Col md="4">
@@ -253,21 +274,44 @@ function PdfDownload({ data }) {
             </ReactToPdf>
             <Form>
               <Form.Group className='mt-2'>
+                <Form.Label>Enter Employee Name</Form.Label>
+                <Form.Control required type="text" placeholder={`Enter name`} onChange={ e => setHostName(e.target.value) } value={ hostName }/>
+                <Form.Control.Feedback type="invalid">
+                  Please enter employee name.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className='mt-2'>
                 <Form.Label>Enter Customer PIC</Form.Label>
-                <Form.Control required type="text" placeholder={`Enter name`} onChange={ e => data.pic = e.target.value } value={ data.pic }/>
+                <Form.Control required type="text" placeholder={`Enter name`} onChange={ e => setName(e.target.value) } value={ name }/>
                 <Form.Control.Feedback type="invalid">
                   Please enter customer name.
                 </Form.Control.Feedback>
               </Form.Group>
               <Form.Group className='mt-2'>
                 <Form.Label>Enter Customer Address</Form.Label>
-                <Form.Control required as="textarea" rows="5" placeholder={`Enter name`} onChange={ e => data.address = e.target.value } value={ data.address }/>
+                <Form.Control required as="textarea" rows="5" placeholder={`Enter name`} onChange={ e => setAddress(e.target.value) } value={ address }/>
                 <Form.Control.Feedback type="invalid">
                   Please enter customer name.
                 </Form.Control.Feedback>
               </Form.Group>
+              <Form.Group className='mt-2'>
+                <Form.Label>Enter Discount Final</Form.Label>
+                <Form.Control required type="number" rows="5" placeholder={`Enter number`} onChange={ handleDiscountFinal } value={ discountFinal }/>
+                <Form.Control.Feedback type="invalid">
+                  Please enter final discount.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group className='mt-2'>
+                <Form.Label>Enter Delivery Fee</Form.Label>
+                <Form.Control required type="number" rows="5" placeholder={`Enter number`} onChange={ e => setDelivery(e.target.value) } value={ delivery }/>
+                <Form.Control.Feedback type="invalid">
+                  Please enter delivery.
+                </Form.Control.Feedback>
+              </Form.Group>
             </Form>
-            <span>{JSON.stringify(data)}</span>
+            <Col>
+              <span>{JSON.stringify(data)}</span>
+            </Col>
           </Container>
         </Col>
       </Row>
